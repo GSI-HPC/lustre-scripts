8cac42dac8d1be6009e9cea6374d6457d95783fd
Gabriele Iannetti <g.iannetti@gsi.de>
2018-10-18 15:15:53 +0200

    Refactored code indentations in collect-rbh-acct-stats.py

--- bin/collect-rbh-acct-stats.py
+++ bin/collect-rbh-acct-stats.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2017 Gabriele Iannetti <g.iannetti@gsi.de>
+# Copyright 2018 Gabriele Iannetti <g.iannetti@gsi.de>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -29,63 +29,61 @@ import smtplib
 from contextlib import closing
 
 
+def raise_option_not_found(section, option):
+   raise Exception("Option: %s not found in section: %s." % (option, section))
 
 
-def raise_option_not_found( section, option ):
-   raise Exception( "Option: " + option + " was not found in section: " + section )
+def validate_config_file(config):
+   
+   if not config.has_section('mysqld'):
+      raise ConfigParser.NoSectionError('Section mysqld was not found!')
 
+   if not config.has_option('mysqld', 'host'):
+      raise_option_not_found('mysqld', 'host')
 
-def validate_config_file( config ):
+   if not config.has_option('mysqld', 'user'):
+      raise_option_not_found('mysqld', 'user')
    
-   if not config.has_section( 'mysqld' ):
-      raise ConfigParser.NoSectionError( 'Section mysqld was not found!' )
-
-   if not config.has_option( 'mysqld', 'host' ):
-      raise_option_not_found( 'mysqld', 'host' )
-
-   if not config.has_option( 'mysqld', 'user' ):
-      raise_option_not_found( 'mysqld', 'user' )
+   if not config.has_option('mysqld', 'password'):
+      raise_option_not_found('mysqld', 'password')
    
-   if not config.has_option( 'mysqld', 'password' ):
-      raise_option_not_found( 'mysqld', 'password' )
+   if not config.has_section('history'):
+      raise ConfigParser.NoSectionError('Section history was not found!')
    
-   if not config.has_section( 'history' ):
-      raise ConfigParser.NoSectionError( 'Section history was not found!' )
+   if not config.has_option('history', 'database'):
+      raise_option_not_found('history', 'database')
    
-   if not config.has_option( 'history', 'database' ):
-      raise_option_not_found( 'history', 'database' )
+   if not config.has_option('history', 'acct_stat_history_table'):
+      raise_option_not_found('history', 'acct_stat_history_table')
    
-   if not config.has_option( 'history', 'acct_stat_history_table' ):
-      raise_option_not_found( 'history', 'acct_stat_history_table' )
+   if not config.has_section('robinhood'):
+      raise ConfigParser.NoSectionError('Section robinhood was not found!')
    
-   if not config.has_section( 'robinhood' ):
-      raise ConfigParser.NoSectionError( 'Section robinhood was not found!' )
+   if not config.has_option('robinhood', 'database'):
+      raise_option_not_found('robinhood', 'database')
    
-   if not config.has_option( 'robinhood', 'database' ):
-      raise_option_not_found( 'robinhood', 'database' )
-   
-   if not config.has_option( 'robinhood', 'acct_stat_table' ):
-      raise_option_not_found( 'robinhood', 'acct_stat_table' )
+   if not config.has_option('robinhood', 'acct_stat_table'):
+      raise_option_not_found('robinhood', 'acct_stat_table')
    
    if not config.has_section( 'mail' ):
       raise ConfigParser.NoSectionError( 'Section mail was not found!' )
    
-   if not config.has_option( 'mail', 'server' ):
-      raise_option_not_found( 'mail', 'server' )
+   if not config.has_option('mail', 'server'):
+      raise_option_not_found('mail', 'server')
    
-   if not config.has_option( 'mail', 'sender' ):
-      raise_option_not_found( 'mail', 'sender' )
+   if not config.has_option('mail', 'sender'):
+      raise_option_not_found('mail', 'sender')
    
-   if not config.has_option( 'mail', 'recipient' ):
-      raise_option_not_found( 'mail', 'recipient' )
+   if not config.has_option('mail', 'recipient'):
+      raise_option_not_found('mail', 'recipient')
 
 
 def create_acct_history_table( cur, db, table ):
    
    sql = "USE " + db
 
-   logging.debug( sql )
-   cur.execute( sql )
+   logging.debug(sql)
+   cur.execute(sql)
    
    sql = """
 CREATE TABLE """ + table + """ (
@@ -98,24 +96,35 @@ CREATE TABLE """ + table + """ (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1
 """
    
-   cur.execute( sql )
-   logging.debug( "Created table:\n" + sql )
+   cur.execute(sql)
+   logging.debug("Created table:\n%s" % sql)
 
 
-def take_acct_stat_snapshot( cur, rbh_db, rbh_acct_table, history_db, history_acct_table, date ):
+def take_acct_stat_snapshot(cur, 
+                            rbh_db, 
+                            rbh_acct_table, 
+                            history_db, 
+                            history_acct_table, 
+                            date):
+       
+   sql = "INSERT INTO %s.%s " \
+         "SELECT uid, gid, SUM(size), SUM(count), '%s' " \
+         "FROM %s.%s " \
+         "WHERE type='file' " \
+         "GROUP BY 1,2" \
+      % (history_db, history_acct_table, date, rbh_db, rbh_acct_table)
    
-   sql = "INSERT INTO " + history_db + "." + history_acct_table + " SELECT uid, gid, SUM(size), SUM(count), '" + date + "' FROM " + rbh_db + "." + rbh_acct_table + " WHERE type='file' GROUP BY 1,2"
-   
-   logging.debug( sql )
-   cur.execute( sql )
+   logging.debug(sql)
+   cur.execute(sql)
    
    if not cur.rowcount:
-      raise RuntimeError( "Failed to take a snapshot for table: " + history_acct_table + " with date: " + date )
+      raise RuntimeError("Snapshot failed for date: %s." % date)
    
-   logging.info( "Inserted rows: " + str( cur.rowcount ) + " into table: " + history_acct_table + " for date: " + date )
+   logging.info("Inserted rows: %d into table: %s for date: %s" 
+      % (cur.rowcount, history_acct_table, date))
 
 
-def create_mail( sender, subject, receiver, text ):
+def create_mail(sender, subject, receiver, text):
    
    mail = """From: <""" + sender + """> 
 To: <""" + receiver + """>
@@ -128,73 +137,93 @@ Subject: """ + subject + """
 
 def main():
 
-   parser = argparse.ArgumentParser( description='Fills accounting history table.' )
-   parser.add_argument( '-f', '--config-file', dest='config_file', type=str, required=True, help='Path of the config file.' )
-   parser.add_argument( '-D', '--enable-debug', dest='enable_debug', required=False, action='store_true', help='Enables logging of debug messages.' )
-   parser.add_argument( '--create-table', dest='create_table', required=False, action='store_true', help='If set the accounting history table is created.' )
+   parser = argparse.ArgumentParser(description='Fills RBH acct history table.')
+
+   parser.add_argument('-f', '--config-file', dest='config_file', type=str, 
+      required=True, help='Path of the config file.')
+   
+   parser.add_argument('-D', '--enable-debug', dest='enable_debug', 
+      required=False, action='store_true', 
+      help='Enables logging of debug messages.')
+
+   parser.add_argument('--create-table', dest='create_table', 
+      required=False, action='store_true', 
+      help='If set the accounting history table is created.')
    
    args = parser.parse_args()
    
-   if not os.path.isfile( args.config_file ):
-      raise IOError( "The config file does not exist or is not a file: " + args.config_file )
+   if not os.path.isfile(args.config_file):
+      raise IOError("The config file does not exist or is not a file: %s" 
+         % args.config_file)
    
    logging_level = logging.INFO
    
    if args.enable_debug:
       logging_level = logging.DEBUG
 
-   logging.basicConfig( level=logging_level, format='%(asctime)s - %(levelname)s: %(message)s' )
+   logging.basicConfig(level=logging_level, 
+      format='%(asctime)s - %(levelname)s: %(message)s')
 
-   logging.info( 'START' )
+   logging.info('START')
 
    config = ConfigParser.ConfigParser()
-   config.read( args.config_file )
+   config.read(args.config_file)
    
-   validate_config_file( config )
+   validate_config_file(config)
    
    try:
       
-      rbh_db         = config.get( 'robinhood', 'database' )
-      rbh_acct_table = config.get( 'robinhood', 'acct_stat_table' )
+      rbh_db = config.get('robinhood', 'database')
+      rbh_acct_table = config.get('robinhood', 'acct_stat_table')
       
-      history_db         = config.get( 'history', 'database' )
-      history_acct_table = config.get( 'history', 'acct_stat_history_table' )
+      history_db = config.get('history', 'database')
+      history_acct_table = config.get('history', 'acct_stat_history_table')
       
-      with closing( MySQLdb.connect( host=config.get( 'mysqld', 'host' ), user=config.get( 'mysqld', 'user' ), passwd=config.get( 'mysqld', 'password' ), db=rbh_db ) )  as conn:
-         with closing( conn.cursor() ) as cur:
-            
-            conn.autocommit( True )
+      with closing(MySQLdb.connect(host=config.get('mysqld', 'host'), 
+                                   user=config.get('mysqld', 'user'), 
+                                   passwd=config.get('mysqld', 'password'), 
+                                   db=rbh_db)) as conn:
+
+         with closing(conn.cursor()) as cur:
+               
+            conn.autocommit(True)
                
             if args.create_table:
-               create_acct_history_table( cur, history_db, history_acct_table )
+               create_acct_history_table(cur, history_db, history_acct_table)
             
-            date_today = time.strftime( '%Y-%m-%d' )
+            date_today = time.strftime('%Y-%m-%d')
             
-            take_acct_stat_snapshot( cur, rbh_db, rbh_acct_table, history_db, history_acct_table, str ( date_today ) )
+            take_acct_stat_snapshot(cur, 
+                                    rbh_db, 
+                                    rbh_acct_table, 
+                                    history_db, 
+                                    history_acct_table, 
+                                    str (date_today))
       
-      logging.info( 'END' )
+      logging.info('END')
       
       return 0
    
    except Exception as e:
       
-      error_msg = str( e )
+      error_msg = str(e)
       
-      logging.error( error_msg )
+      logging.error(error_msg)
       
-      mail_server    = config.get( 'mail', 'server' )
-      mail_sender    = config.get( 'mail', 'sender' )
-      mail_recipient = config.get( 'mail', 'recipient' )
+      mail_server = config.get('mail', 'server')
+      mail_sender = config.get('mail', 'sender')
+      mail_recipient = config.get('mail', 'recipient')
       
       mail_subject = __file__ + " - Error Occured!"
-      mail_text    = error_msg
+      mail_text = error_msg
       
-      mail = create_mail( mail_sender, mail_subject, mail_recipient, mail_text )
+      mail = create_mail(mail_sender, mail_subject, mail_recipient, mail_text)
       
-      smtp_conn = smtplib.SMTP( mail_server )
-      smtp_conn.sendmail( mail_sender, mail_recipient, mail )
+      smtp_conn = smtplib.SMTP(mail_server)
+      smtp_conn.sendmail(mail_sender, mail_recipient, mail)
       
-      logging.info( "Error notification mail has been sent to: " + mail_recipient )
+      logging.info("Error notification mail has been sent to: %s" % 
+         mail_recipient)
       
       smtp_conn.quit()
 
